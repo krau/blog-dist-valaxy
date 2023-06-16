6fe7462869d007e3e7cd08fb9bd6b3ab860d9674
krau <krau@users.noreply.github.com>
2023-06-16 02:56:10 +0000

    deploy: krau/blog-source-valaxy@f7dc40cbc22f98be2eab52da8f3aae1b4763968f

---
+++ assets/_...all_-b8649b2a.js
@@ -0,0 +1,1 @@
+import{g as a,a3 as s,a4 as n,a5 as r,o as c,i as p}from"./app-decf7611.js";import{b as o}from"./route-block-83d24a4e.js";const u=a({__name:"[...all]",setup(i){const t=s(),e=n();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&t.replace(e.path.slice(0,-1))}),(f,l)=>(c(),p("div"))}});typeof o=="function"&&o(u);export{u as default};
