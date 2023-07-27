ecd4d8774937107a1981feed82faf32665a95445
krau <krau@users.noreply.github.com>
2023-07-27 12:31:19 +0000

    deploy: krau/blog-source-valaxy@20882b125fff65e0070b3e49d692819c18bc030a

---
+++ assets/YunGallery-8007cd76.js
@@ -0,0 +1,1 @@
+import{g as n,h as s,o as t,c as p}from"./app-fec6569f.js";const c=n({__name:"YunGallery",props:{photos:{}},setup(r){return(o,a)=>{const e=s("VAGallery");return t(),p(e,{photos:o.photos},null,8,["photos"])}}});export{c as default};
