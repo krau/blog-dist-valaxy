d47bbb6283306141fbfabf952bf5ab3f047781f5
krau <krau@users.noreply.github.com>
2024-06-11 12:44:45 +0000

    deploy: krau/blog-source-valaxy@47aa1c327c72d16758529e7dee69e3697290d0cf

---
+++ assets/helper-BmZ-QVgq.js
@@ -0,0 +1,1 @@
+import{R as i,ad as s,K as f}from"./app-CHU2jPRP.js";function m(a,r=!1){const e=i();return s(()=>a,async()=>{let t;if(typeof a=="string"){if(!f)return;t=await fetch(a).then(n=>n.json())||[]}else t=a;e.value=r?Array.from(t).sort(()=>Math.random()-.5):t},{immediate:!0}),{data:e}}export{m as u};
