800915499e2538afb600465d18a174f4ababa458
krau <krau@users.noreply.github.com>
2024-06-11 12:44:45 +0000

    deploy: krau/blog-source-valaxy@47aa1c327c72d16758529e7dee69e3697290d0cf

---
+++ assets/post-ChrFDlsk.js
@@ -0,0 +1,1 @@
+import{A as c,l as i}from"./app-CHU2jPRP.js";function u(o){if(!o)return{color:"",icon:"",styles:void 0};const r=c();o in r.value.types||(o="link");const s=r.value.types[o].color,n=r.value.types[o].icon,e=i(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:n,styles:e}}export{u};
