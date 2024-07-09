898fa375c76b713c9fbc3365bf3860ff9deff775
krau <krau@users.noreply.github.com>
2024-07-09 05:21:26 +0000

    deploy: krau/blog-source-valaxy@cb0d8be41dde9033773778ee8204bf25e6c93959

---
+++ assets/back-BCiFZjmV.js
@@ -0,0 +1,1 @@
+import{i as o}from"./app-D4mOEXGx.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
---
+++ assets/helper-Dpa8wEYU.js
@@ -0,0 +1,1 @@
+import{W as i,ao as l,ap as t}from"./app-D4mOEXGx.js";function r(e){const s=i(!1),{top:o}=l(e);return t(e,([{isIntersecting:n}])=>{s.value=n}),{show:()=>{s.value||window.scrollTo(0,o.value)}}}export{r as u};
