a7c33c2c3f52f33cdbf9c9486a32775b0288d493
krau <krau@users.noreply.github.com>
2024-07-09 05:21:26 +0000

    deploy: krau/blog-source-valaxy@cb0d8be41dde9033773778ee8204bf25e6c93959

---
+++ assets/helper-C_YwPkna.js
@@ -0,0 +1,1 @@
+import{W as i,a8 as s,L as f}from"./app-D4mOEXGx.js";function m(a,r=!1){const e=i();return s(()=>a,async()=>{let t;if(typeof a=="string"){if(!f)return;t=await fetch(a).then(n=>n.json())||[]}else t=a;e.value=r?Array.from(t).sort(()=>Math.random()-.5):t},{immediate:!0}),{data:e}}export{m as u};
